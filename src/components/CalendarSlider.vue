a904cf05270e1db2ead5cd5755d08401e8fe021e
Kevin Kirchner <kirchner.kevin@gmail.com>
2018-07-06 11:29:30 -0500

    done or not done icons added

--- src/components/CalendarSlider.vue
+++ src/components/CalendarSlider.vue
@@ -2,32 +2,47 @@
   <div class="calendar-slider">
     <div class="inner">
       <div @click="setWorkoutDate(-2)" :class="['workout', 'two-days-out', {'is-today': twoDaysBeforeIsToday}]">
-        <div class="icon">&nbsp;</div>
+        <div class="icon">
+          <check-icon v-if="twoDaysBeforeIsDone" class="is-done" />
+          <times-icon v-else class="isnt-done" />
+        </div>
         <div class="week-day">{{ twoDaysBeforeWeekDay }}</div>
         <div class="date">{{ twoDaysBefore }}</div>
         <div class="workout-title">{{ twoDaysBeforeWorkout.title }}</div>
       </div>
       <div @click="setWorkoutDate(-1)" :class="['workout', 'one-day-out', {'is-today': oneDayBeforeIsToday}]">
-        <div class="icon">&nbsp;</div>
+        <div class="icon">
+          <check-icon v-if="oneDayBeforeIsDone" class="is-done" />
+          <times-icon v-else class="isnt-done" />
+        </div>
         <div class="week-day">{{ oneDayBeforeWeekDay }}</div>
         <div class="date">{{ oneDayBefore }}</div>
         <div class="workout-title">{{ oneDayBeforeWorkout.title }}</div>
       </div>
       <div :class="['workout', 'workout-day', {'is-today': workoutDayIsToday}]">
-        <div class="icon">&nbsp;</div>
+        <div class="icon">
+          <check-icon v-if="workoutDayIsDone" class="is-done" />
+          <times-icon v-else class="isnt-done" />
+        </div>
         <div class="week-day">{{ workoutWeekDay }}</div>
         <div class="date">{{ workoutDay }}</div>
         <h1 class="workout-title">{{ workout.title }}</h1>
         <h1 class="workout-body">{{ workout.body }}</h1>
       </div>
       <div @click="setWorkoutDate(1)" :class="['workout', 'one-day-out', {'is-today': oneDayAfterIsToday}]">
-        <div class="icon">&nbsp;</div>
+        <div class="icon">
+          <check-icon v-if="oneDayAfterIsDone" class="is-done" />
+          <times-icon v-else class="isnt-done" />
+        </div>
         <div class="week-day">{{ oneDayAfterWeekDay }}</div>
         <div class="date">{{ oneDayAfter }}</div>
         <div class="workout-title">{{ oneDayAfterWorkout.title }}</div>
       </div>
       <div @click="setWorkoutDate(2)" :class="['workout', 'two-days-out', {'is-today': twoDaysAfterIsToday}]">
-        <div class="icon">&nbsp;</div>
+        <div class="icon">
+          <check-icon v-if="twoDaysAfterIsDone" class="is-done" />
+          <times-icon v-else class="isnt-done" />
+        </div>
         <div class="week-day">{{ twoDaysAfterWeekDay }}</div>
         <div class="date">{{ twoDaysAfter }}</div>
         <div class="workout-title">{{ twoDaysAfterWorkout.title }}</div>
@@ -37,14 +52,26 @@
 </template>
 
 <script>
+import Vue from 'vue';
 import { WORKOUTS, WORKOUT_PLACEHOLDER } from '../workouts';
+const CheckIcon = Vue.component(
+  'check-icon',
+  require('@/assets/images/check-regular.svg')
+);
+const TimesIcon = Vue.component(
+  'times-icon',
+  require('@/assets/images/times-regular.svg')
+);
+
 export default {
   name: 'CalendarSlider',
-
+  components: {
+    CheckIcon,
+    TimesIcon,
+  },
   data() {
     return {};
   },
-
   computed: {
     twoDaysBefore() {
       return this.$store.getters.twoDatesBefore.getDate();
@@ -75,6 +102,21 @@ export default {
     },
     twoDaysAfterIsToday() {
       return this.isToday(this.$store.getters.twoDatesAfter);
+    },
+    twoDaysBeforeIsDone() {
+      return this.workoutIsDone(this.$store.getters.twoDatesBefore);
+    },
+    oneDayBeforeIsDone() {
+      return this.workoutIsDone(this.$store.getters.oneDateBefore);
+    },
+    workoutDayIsDone() {
+      return this.workoutIsDone(this.$store.state.workoutDate);
+    },
+    oneDayAfterIsDone() {
+      return this.workoutIsDone(this.$store.getters.oneDateAfter);
+    },
+    twoDaysAfterIsDone() {
+      return this.workoutIsDone(this.$store.getters.twoDatesAfter);
     },
     twoDaysBeforeWeekDay() {
       return this.$store.getters.twoDatesBefore.getWeekDay().substr(0, 1);
@@ -130,6 +172,11 @@ export default {
       return (
         new Date().toISODateString() === new Date(workoutDate).toISODateString()
       );
+    },
+    workoutIsDone(workoutDate) {
+      let workoutDateISO = new Date(workoutDate).toISODateString();
+      let workout = this.$store.state.workouts[workoutDateISO] || {};
+      return workout.isDone;
     },
   },
 };
