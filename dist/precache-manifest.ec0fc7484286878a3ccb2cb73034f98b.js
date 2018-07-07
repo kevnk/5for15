5e50b971d7a25c05c9e0d34c757a4d398a605df7
Kevin Kirchner <kirchner.kevin@gmail.com>
2018-07-06 22:42:58 -0500

    committing dist

---
+++ dist/precache-manifest.ec0fc7484286878a3ccb2cb73034f98b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9483e133d099261fdf96",
+    "url": "/js/chunk-vendors.9483e133.js"
+  },
+  {
+    "revision": "366a517566152ffe5545",
+    "url": "/js/app.366a5175.js"
+  },
+  {
+    "revision": "d139b70b5a887932b666657492490e1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "366a517566152ffe5545",
+    "url": "/css/app.f59ad869.css"
+  }
+];
